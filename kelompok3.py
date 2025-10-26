aec6aed078a72363215afa1eb0cbb962b2cda2e3
Almira Sadida <almira244@mhs.usk.ac.id>
2025-10-26 21:23:28 +0700

    Update kelompok3.py
    mengimport Library numpy dan membuat daftar panduan input

--- kelompok3.py
+++ kelompok3.py
@@ -1,1 +1,15 @@
+# ======================================
+# METODE SECANT - PENCARIAN AKAR PERSAMAAN NON-LINEAR
+# ======================================
 
+# Import library
+import numpy as np
+
+# ================== PANDUAN INPUT ==================
+print("="*70)
+print("METODE SECANT - PENCARIAN AKAR PERSAMAAN NON-LINEAR")
+print("="*70)
+print("\nPANDUAN PENULISAN PERSAMAAN f(x):")
+print("-" * 70)
+print("• Gunakan 'x' sebagai variabel")
+print("• Pangkat (polinomial)        : x**2, x**3, dst")
